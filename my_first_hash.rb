dfd9499ce571546fd2f590377aea1a28a733d927
michael otero <michaelotero1118@gmail.com>
2020-02-03 03:39:29 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,1 +1,46 @@
 
+def my_hash
+
+my_hash = {"name" => "Alvaro", "age" => 27 }
+
+end
+
+ 
+
+ 
+
+def shipping_manifest
+
+the_manifest = {"whale bone corsets" => 5, "oil paintings" => 2, "porcelain vases" => 3}
+
+ 
+
+end
+
+ 
+
+def retrieval
+
+  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+
+  shipping_manifest["oil paintings"]
+
+end
+
+ 
+
+def adding
+
+  shipping_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3 }
+
+ 
+
+  shipping_manifest["muskets"] = 2
+
+   shipping_manifest["gun powder"] = 4
+
+  
+
+  shipping_manifest
+
+end
